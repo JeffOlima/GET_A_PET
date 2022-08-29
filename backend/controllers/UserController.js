0d906a0495adbaf5c993988a0d1c0a80043e3c10
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-08-29 20:56:53 +0100

    Register validation added

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,6 +2,43 @@ const User = require('../models/User')
 
 module.exports = class UserController{
     static async register(req, res){
-        res.json('hello Get A Pet')
+       const {name, email, phone, password, confirmpassword} =req.body
+    
+       //Validations
+       if(!name){
+           res.status(422).json({ message: 'Name required !'})
+           return
+       } 
+       if(!email){
+        res.status(422).json({ message: 'Email required !'})
+        return
+       } 
+       if(!phone){
+        res.status(422).json({ message: 'Phone required!'})
+        return
+       } 
+       if(!password){
+        res.status(422).json({ message: 'Password required!'})
+        return
+       } 
+       if(!confirmpassword){
+        res.status(422).json({ message: 'Confirmation required!'})
+        return
+       } 
+       if(password !==confirmpassword){
+        res.status(422).json({
+             message: 'password and password confirmation do not match!'
+            })
+        return
+       }
+       
+       //check if user exists 
+       const userExists = await User.findOne({ email: email})
+        
+       if(userExists){
+           res.status(422).json({
+               message: 'This email already exists on the system!'
+           })
+       }
     }
 }
