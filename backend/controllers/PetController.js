15431a6c1cab869a266dc73ca024cba6973d1905
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-02 22:13:40 +0100

    Upload od multiple images

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,9 +1,74 @@
 const Pet = require('../models/Pet') 
 
+//helpers
+const getToken = require("../helpers/get-token")
+const getUserByToken = require("../helpers/get-user-by-token")
+
 module.exports = class PetController {
     //create a pet
     static async create(req, res) {
-        res.json({ message: 'It worked!'})
+        const { name, age, weight, color } = req.body
 
+        const images = req.files
+
+        const available = true 
+
+        //images upload 
+
+        // validations 
+        if(!name){
+            res.status(422).json({ mesage: "Name required"})
+            return
+        }
+        if(!age){
+            res.status(422).json({ mesage: "Age required"})
+            return
+        }
+        if(!weight){
+            res.status(422).json({ mesage: "weight required"})
+            return
+        }
+        if(!color){
+            res.status(422).json({ mesage: "color required"})
+            return
+        }
+        if(images.length === 0){
+            res.status(422).json({ message: "Images required"})
+            return
+        }
+        // get pet owner
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        // create a pet 
+        const pet = new Pet({
+            name,
+            age,
+            weight,
+            color,
+            available,
+            images:[],
+            user: {
+               _id: user._id,
+               name: user.name,
+               image: user.image,
+               phone: user.phone, 
+            },
+        })
+
+        images.map((image) => {
+            pet.images.push(image.filename)
+        })
+          
+
+        try {
+            const newPet = await pet.save()
+            res.status(201).json({
+                message: 'Pet registered successfully!',
+                newPet,
+            })
+        } catch (error) {
+            res.status(500).json({ message: error})
+        }
     }
 }
