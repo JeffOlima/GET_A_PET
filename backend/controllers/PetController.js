f78f1699eed0b36cc3e0ad41939cf742e08fb5c4
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-04 13:33:16 +0100

    Remove pet from system

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -110,6 +110,7 @@ module.exports = class PetController {
     static async getPetById(req, res){
         const id = req.params.id
 
+        //check if id is valid
         if(!ObjectId.isValid(id)){
             res.status(422).json({ message: 'Invalid ID'})
             return
@@ -125,5 +126,36 @@ module.exports = class PetController {
         res.status(200).json({
             pet: pet,
         })
+    }
+
+    static async removePetById(req, res){
+        const id = req.params.id
+
+        //check if id is valid
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({ message: 'Invalid id!'})
+            return
+        }
+
+        //check if pet exists 
+        const pet = await Pet.findOne({_id: id})
+
+        if (!pet){
+            res.status(404).json({ message : 'Pet not found!'})
+        }
+
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        if (pet.user._id.toString() !== user._id.toString()){
+            res.status(422)
+            .json({
+                message: "We are facing a problem try it latter!"
+            })
+        }
+
+        await Pet.findByIdAndRemove(id)
+
+        res.status(200).json({ message: 'Pet removed successfully!'})
     }
 }
