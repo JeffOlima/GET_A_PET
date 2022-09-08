5a8486a2be5791b20aa6970f73dd9baa72d144b1
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-08 13:31:50 +0100

    Installing react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+/* pages */
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Home from './components/pages/Home'
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Get A Pet</h1>
-      </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
