770f6f2305ff22bb3c31a10f2ec27c3c180b1309
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-08 21:46:04 +0100

    Applying form styles

--- frontend/src/components/pages/Auth/Register.js
+++ frontend/src/components/pages/Auth/Register.js
@@ -1,7 +1,56 @@
+import Input from '../../form/Input'
+import {Link} from 'react-router-dom'
+
+import styles from '../../form/Form.module.css'
+
 function Register(){
+
+    function handleChange(e){}
+
     return(
-        <section>
+        <section className={styles.form_container}>
             <h1>Register</h1>
+            <form>
+                <Input
+                text="Name"
+                type="text"
+                name="name"
+                placeholder='Type your name'
+                handleOnChange={handleChange}
+                />
+                <Input
+                text="Phone"
+                type="text"
+                name="phone"
+                placeholder='Type your phone'
+                handleOnChange={handleChange}
+                />
+                <Input
+                text="E-mail"
+                type="email"
+                name="email"
+                placeholder='Type your e-mail'
+                handleOnChange={handleChange}
+                />
+                <Input
+                text="Password"
+                type="password"
+                name="password"
+                placeholder='Type your password'
+                handleOnChange={handleChange}
+                />
+                <Input
+                text="Password confirmation"
+                type="password"
+                name="confirmpassword"
+                placeholder='Type your password'
+                handleOnChange={handleChange}
+                />
+                <input type="submit" value="Register"/>
+            </form>
+            <p>
+                Are you alredy registered <Link to="/login">Click here.</Link>
+            </p>
         </section>
     )
 }
