259d50d5d357a01db331bcd9731037c986f5f137
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-09 18:26:17 +0100

    Finishing flash message component

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,17 +2,31 @@ import api from '../utils/api'
 
 import {useState, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
+import useFlashMessage from './useFlashMessage'
 
 export default function useAuth() {
+
+    const {setFlashMessage} = useFlashMessage()
+
     async function register(user){
+
+        let msgText = 'Registered successfully!'
+        let msgType = 'success'
+
         try {
-            const data = await api.post('/users/register', user).then((responses) =>{
-                return responses.data
+            const data = await api.post('/users/register', user).then((response) =>{
+                return response.data
             })
+            
             console.log(data)
         } catch (error) {
-            // error
+
+             msgText = error.response.data.message
+             msgType = 'error'
         }
+
+        setFlashMessage(msgText, msgType)
+
     }
     return {register}
 } 
