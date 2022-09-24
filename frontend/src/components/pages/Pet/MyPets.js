f3e53149169627e93f650150dbe8adbc354fe64c
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-09-24 21:33:34 +0100

    deleting pet from system

--- frontend/src/components/pages/Pet/MyPets.js
+++ frontend/src/components/pages/Pet/MyPets.js
@@ -27,28 +27,50 @@ function MyPets(){
           })
       }, [token])
 
+      async function removePet(id){
+        let msgType = 'success'
+
+        const data = await api.delete(`/pets/${id}`,{
+          headers:{
+            Authorization: `Bearer ${JSON.parse(token)}`,
+          }
+        }).then((response) =>{
+          const updatedPets = pets.filter((pet) => pet._id !=id)
+          setPets(updatedPets)
+          return response.data
+        })
+        .catch((err) =>{
+          msgType = 'error'
+          return err.response.data
+        })
+        setFlashMessage(data.message, msgType)
+      } 
+
+
     return(
         <section>
-           <div>
+           <div className={styles.petlist_header}>
            <h1>MyPets</h1>
            <Link to="/pet/add">Register a new Pet</Link>
            </div>
             <div>
             {pets.length > 0 &&
           pets.map((pet) => (
-            <div key={pet._id} >
+            <div className={styles.petlist_row} key={pet._id} >
               <RoundedImage
                 src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
                 alt={pet.name}
                 width="px75"
               />
               <span className="bold">{pet.name}</span>
-              <div className={styles.action}>
+              <div className={styles.actions}>
                 {pet.available ? (
                     <>
-                    {pet.adopter && <button>Coclud adoption</button>}
+                    {pet.adopter && <button className={styles.conclude_btn}>Coclud adoption</button>}
                     <Link to={`/pet/edit/${pet._id}`}>Edit</Link>
-                    <button>Delete</button>
+                    <button onClick={() =>{
+                      removePet(pet._id)
+                    }}>Delete</button>
                     </>
                 ) : <p>Pet already adopted</p>}
               </div>
