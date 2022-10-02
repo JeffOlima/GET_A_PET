c889579bf118c20645f4daeca7652b9d4f96e925
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-10-03 00:39:31 +0100

    Showing pets on the home page

--- frontend/src/components/form/PetForm.js
+++ frontend/src/components/form/PetForm.js
@@ -1,96 +1,98 @@
-import { useState} from 'react'
+import { useState } from 'react'
 
 import formStyles from './Form.module.css'
 
 import Input from './Input'
 import Select from './Select'
 
+function PetForm({ handleSubmit, petData, btnText }) {
+  const [pet, setPet] = useState(petData || {})
+  const [preview, setPreview] = useState([])
+  const colors = ['Branco', 'Preto', 'Cinza', 'Caramelo']
 
-function PetForm({handleSubmit, petData, btnText}){
-    const [pet, setPet] = useState(petData || {})
-    const [preview, setPreview] = useState([])
-    const colors = ['White', 'Black', 'Brown', 'Grey']
-
-function onFileChange(e){
+  function onFileChange(e) {
+    console.log(Array.from(e.target.files))
     setPreview(Array.from(e.target.files))
-    setPet({...pet, images:[...e.target.files]})
-}
+    setPet({ ...pet, images: [...e.target.files] })
+  }
 
-function handleChange(e){
-    setPet({...pet, [e.target.name]: e.target.value})
-}
+  function handleChange(e) {
+    setPet({ ...pet, [e.target.name]: e.target.value })
+  }
 
-function handleColor (e) {
-    setPet({...pet, color: e.target.options[e.target.selectedIndex].text})
-}
+  function handleColor(e) {
+    setPet({
+      ...pet,
+      color: e.target.options[e.target.selectedIndex].text,
+    })
+  }
 
-function submit(e){
+  const submit = (e) => {
     e.preventDefault()
     handleSubmit(pet)
-}
+  }
 
-    return(
-        <form onSubmit= {submit} className={formStyles.form_container}>
-            <div className={formStyles.preview_pet_images}>
-                {preview.length > 0
-                ? preview.map((image, index) => (
-                    <img
-                    src={URL.createObjectURL(image)}
-                    alt={pet.name}
-                    key={`${pet.name}+${index}`}
-                    />
-                ))
-                : pet.images && 
-                pet.images.map((image, index) => (
-                <img
+  return (
+    <form onSubmit={submit} className={formStyles.form_container}>
+      <div className={formStyles.preview_pet_images}>
+        {preview.length > 0
+          ? preview.map((image, index) => (
+              <img
+                src={URL.createObjectURL(image)}
+                alt={pet.name}
+                key={`${pet.name}+${index}`}
+              />
+            ))
+          : pet.images &&
+            pet.images.map((image, index) => (
+              <img
                 src={`${process.env.REACT_APP_API}/images/pets/${image}`}
                 alt={pet.name}
                 key={`${pet.name}+${index}`}
-                />
-                ))}
-            </div>
-            <Input
-              text="Pet image"
-              type="file"
-              name="images"
-              handleOnChange={onFileChange}  
-              multiple={true}            
-            />
-            <Input
-              text="Pet name"
-              type="text"
-              name="name"
-              placeholder="Type pet name"
-              handleOnChange={handleChange}  
-              value={pet.name || ''}            
-            />
-            <Input
-              text="Pet age"
-              type="text"
-              name="age"
-              placeholder="Type pet age"
-              handleOnChange={handleChange}  
-              value={pet.age || ''}            
-            />
-            <Input
-              text="Pet weight"
-              type="number"
-              name="weight"
-              placeholder="Type pet weight"
-              handleOnChange={handleChange}  
-              value={pet.weight || ''}            
-            />
-            <Select
-            name= "color"
-            text= "Select a color"
-            options={colors}
-            handleOnChange={handleColor}
-            value={pet.color || ''}
-            />
-            <input type ="submit" value={btnText}/>
-        </form>
-       
-    )
+              />
+            ))}
+      </div>
+      <Input
+        text="Imagens do Pet"
+        type="file"
+        name="images"
+        handleOnChange={onFileChange}
+        multiple={true}
+      />
+      <Input
+        text="Nome do Pet"
+        type="text"
+        name="name"
+        placeholder="Digite o nome"
+        handleOnChange={handleChange}
+        value={pet.name || ''}
+      />
+      <Input
+        text="Idade do Pet"
+        type="number"
+        name="age"
+        placeholder="Digite a idade"
+        handleOnChange={handleChange}
+        value={pet.age || ''}
+      />
+      <Input
+        text="Peso do Pet"
+        type="number"
+        name="weight"
+        placeholder="Digite o peso aproximado"
+        value={pet.weight || ''}
+        handleOnChange={handleChange}
+      />
+      <Select
+        name="color"
+        text="Selecione a categoria"
+        options={colors}
+        handleOnChange={handleColor}
+        value={pet.color || ''}
+      />
+      <input type="submit" value={btnText} />
+    </form>
+  )
 }
 
 export default PetForm
--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,7 +1,46 @@
+import api from '../../utils/api' 
+
+import {Link} from 'react-router-dom'
+import { useState, useEffect } from 'react'
+
+import styles from './Home.module.css' 
+
 function Home(){
+    const [pets, setPets] = useState([])
+
+    useEffect(() => {
+        api.get('/pets').then((response) =>{
+            setPets(response.data.pets)
+            console.log(pets)
+        })
+    }, [])
+    
     return(
         <section>
-            <h1>Home</h1>
+          <div>
+          <h1>Adopt a pet</h1>
+            <p>See the details of all of them and contatct their owner</p>
+          </div>
+          <div>
+              {pets.length > 0 && pets.map((pet) =>(
+                  <div>
+                      <p>Imagem do Pet</p>
+                      <h3>{pet.name}</h3>
+                      <p>
+                          <span className='bold'>Weight</span> {pet.weight}Kg
+                      </p>
+                      {pet.available ? (
+                          <Link to={`pet/${pet._id}`}>More details</Link>
+                       ) : (
+                        <p>Adopted</p>
+                        )} 
+                  </div>
+             ))}
+              
+              {pets.length === 0 &&(
+                  <p>There are no available pets at the moment!</p>
+              )}
+          </div>
         </section>
     )
 }
