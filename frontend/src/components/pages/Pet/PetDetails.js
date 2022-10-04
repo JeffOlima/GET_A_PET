3bc9ef09093f059e9538b3dc5eae7cc45617ff92
Jefferson Lima <oliveiralima.jefferson@gmail.com>
2022-10-04 09:22:31 +0100

    Structuring the pet page

--- frontend/src/components/pages/Pet/PetDetails.js
+++ frontend/src/components/pages/Pet/PetDetails.js
@@ -15,13 +15,45 @@ function PetDetails(){
     const [token] = useState(localStorage.getItem('token') || '')
 
     useEffect(() =>{
-        api.get(`/pets${id}`).then((response) =>{
+        api.get(`/pets/${id}`).then((response) =>{
             setPet(response.data.pet)
         })
     }, [id])
     
-    
-    return <h1>{pet.name}</h1>
+    return (
+        <>
+        {pet.name && (
+            <section>
+                <div>
+                    <h1>Getting to know the pet: {pet.name}</h1>
+                    <p>If you are interested, get in touch with the owner to visit it</p>
+                </div>
+                <div>
+                    {pet.images.map((image, index) =>(
+                       <img 
+                       key={index}
+                       src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                        alt={pet.name}
+                       /> 
+                    ))}
+                </div>
+                <p>
+                    <span className='bold'>weight:</span>{pet.weight}Kg
+                </p>
+                <p>
+                    <span className='bold'>Age:</span>{pet.age} years old
+                </p>
+                {token ? (
+                    <button>Schedule a visit</button>
+                 ) : (
+                    <p>
+                       You need <Link to="/register">Create a account</Link> to schedule a visit    
+                    </p>
+                )}
+            </section>
+        )}
+        </>
+    ) 
 }
 
 export default PetDetails
